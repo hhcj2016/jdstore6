f1dee064aec07290b3110c13e9c5cf1cd892c615
Cindy <cindyjiang88@163.com>
2018-01-09 21:38:43 +0800

    add notify_cancel/ship function

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -14,4 +14,20 @@ class OrderMailer < ApplicationMailer
 
    mail(to: "admin@test.com", subject: "[JDStore]用户#{order.user.email}申请取消订单#{order.token}")
   end
+
+  def notify_ship(order)
+    @order = order
+    @user = order.user
+    @product_lists = @order.product_lists
+
+    mail(to: @user.email, subject: "您在[JDStore]的订单#{order.token}已发货")
+  end
+
+  def notify_cancel(order)
+    @order = order
+    @user = order.user
+    @product_lists = @order.product_lists
+
+    mail(to: @user.email, subject: "您在[JDStore]在订单#{order.token}已取消")
+  end
 end
