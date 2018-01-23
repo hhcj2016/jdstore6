0bd94d919d37f099ba71a1403a72f0d13c222457
Cindy <cindyjiang88@163.com>
2018-01-23 11:35:32 +0800

    update seeds etc.

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,16 @@ u.password = "123456"
 u.password_confirmation = "123456"
 u.is_admin = true
 u.save
+
+Product.create!(title: "test",
+                description: "test",
+                price: 20,
+                quantity: 5,
+                image: open("http://om4orurjh.bkt.clouddn.com/uploads/product/image/2/medium_%E7%94%9C%E8%9C%9C.jpg"))
+
+Product.create!(title: "爱似流星",
+                description: "蛋糕",
+                price: 228,
+                quantity: 10,
+                image: open("http://om4orurjh.bkt.clouddn.com/%E7%88%B1%E4%BC%BC%E6%B5%81%E6%98%9F.jpg")
+                )
